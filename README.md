3f4d9ebbf35b26541fe1e4d36e6aa2c734763d33
Brandon <brandon@atomicfi.com>
2024-03-15 10:56:37 -0600

    init: inital commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # md5S3Uploader
+Just playing around with the idea of a package to simplify uploading to S3 buckets with Object Lock enabled. Buckets with Object Lock require MD5 for sender validation. 
---
+++ md5ToS3.go
@@ -0,0 +1,70 @@
+package md5S3Uploader
+
+import (
+	"bytes"
+	"context"
+	"crypto/md5"
+	"encoding/base64"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+type Uploader struct {
+	File   []byte
+	Bucket string
+	Key    string
+}
+
+func getConfig(ctx context.Context) (aws.Config, error) {
+	cfg, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		return aws.Config{}, err
+	}
+	return cfg, err
+}
+
+func makeMd5(ctx context.Context, file []byte) (checksum string, newFile []byte) {
+	if ctx.Done() != nil {
+		h := md5.New()
+		h.Write(file)
+		checksum = base64.StdEncoding.EncodeToString(h.Sum(nil))
+	}
+
+	return checksum, file
+}
+
+func uploadS3(ctx context.Context, cfg aws.Config, file []byte, bucket string, key string) error {
+	// Create an Amazon S3 service client
+	client := s3.NewFromConfig(cfg)
+
+	checksum, file := makeMd5(ctx, file)
+
+	// Create an object
+	_, err := client.PutObject(ctx, &s3.PutObjectInput{
+		Bucket:     &bucket,
+		Key:        &key,
+		Body:       bytes.NewReader(file),
+		ContentMD5: &checksum,
+	})
+	if err != nil {
+		log.Printf("Got an error uploading the file: %v", err)
+	}
+	log.Printf("Successfully uploaded %s to %s\n", key, bucket)
+
+	return err
+}
+
+func (u *Uploader) Upload(ctx context.Context, file []byte, bucket string, key string) error {
+	cfg, err := getConfig(ctx)
+	if err != nil {
+		log.Printf("unable to load AWS config, %v", err)
+	}
+	err = uploadS3(ctx, cfg, file, bucket, key)
+	if err != nil {
+		log.Printf("unable to upload to S3, %v", err)
+	}
+	return err
+}
